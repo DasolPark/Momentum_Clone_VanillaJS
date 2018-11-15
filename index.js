c1938f5bddce18d75df59840a1304b331084a37a
Dasol Park <dasol.park615@gmail.com>
2018-11-15 22:06:03 +0900

    [Theory] var, let, const

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // alert('I am working. I am JS. I am beautiful. I am worth it.')
-console.log('I am working. I am JS. I am beautiful. I am worth it.')
+// console.log('I am working. I am JS. I am beautiful. I am worth it.')
+var a = 221;
+let b = a - 5;
+const c = 777;
+a = 4;
+console.log(b, a, c);
