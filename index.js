fd3d13b857d3411351cefe3ab7dfe98fbfa28a2e
Dasol Park <dasol.park615@gmail.com>
2018-11-17 22:40:53 +0900

    [Theory] Array, Object

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // alert('I am working. I am JS. I am beautiful. I am worth it.')
 // console.log('I am working. I am JS. I am beautiful. I am worth it.')
-var a = 221;
-let b = a - 5;
-const c = 777;
-a = 4;
-console.log(b, a, c);
+// var a = 221;
+// let b = a - 5;
+// const c = 777;
+// a = 4;
+// console.log(b, a, c);
+const dasol = {
+    name: "dasol",
+    age: "21",
+    gender: "Male",
+    FavMovies: ["Oldboy", "Along the gods", "Little forest"],
+    FavFood: [
+        {
+            name: "Kimchi",
+            fatty: false
+        },
+        {
+            name: "Cheese burger",
+            fatty: true
+        }
+    ]
+}
+
+console.log(dasol.FavFood[0].name);
