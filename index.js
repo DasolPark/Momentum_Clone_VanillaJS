5fe933410dbf568460aff6803e8a12eead4870dd
Dasol Park <dasol.park615@gmail.com>
2018-12-03 23:31:53 +0900

    [Practice] 2.4 Events and event handlers
    ("resize", handleResize);
    //내가 원하는 시점에 실행
    ("resize", handleResize());
    //무조건 실행

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const title = document.querySelector("#title");
-// console.dir(title);
-title.innerHTML = "Hi! From JS";
-title.style.color = "red";
-document.title = "I O U"
+
+// function handleResize(event){}
+function handleResize(){
+    // console.log("I hava been resized");
+    // console.log(event);
+    title.style.color = "blue";
+    console.log(window);
+}
+
+window.addEventListener("click", handleResize);
+//resize 이벤트를 기다리다가 내가 필요한 시점에 handleResize함수를 실행
+//handleResize()가 아니라는 것 매우 중요!!
+//handleResize()로 선언하면 지금 바로 호출하라는 것
