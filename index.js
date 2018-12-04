f479845e76915d9ad4e58bb3f023a4a827031ed1
Dasol Park <dasol.park615@gmail.com>
2018-12-04 17:53:53 +0900

    [Practice] 2.7 Dom - If else - Function practice part Two
    ing...

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";//"#34495e";
-const OTHER_COLOR = "rgba(51, 110, 123, 1)";
+const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentColor = title.style.color;
-    if(currentColor == BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    }else{
-        title.style.color = BASE_COLOR;
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    } else {
+        title.className = "";
     }
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
 init();
-
-function handleOffline(){
-    console.log("I'm off");
-}
-function handleOnline(){
-    console.log("Welcome back!");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
