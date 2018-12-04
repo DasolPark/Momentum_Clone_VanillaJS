a983c4575d8e808a4f27ca73371443f036d72eb3
Dasol Park <dasol.park615@gmail.com>
2018-12-04 22:34:05 +0900

    [Practice] 2.7 DOM - If else - Function practice part Two
    how to use classList

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentClass = title.className;
-    if(currentClass !== CLICKED_CLASS){
-        title.className = CLICKED_CLASS;
-    } else {
-        title.className = "";
-    }
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(hasClass){
+    //     title.classList.remove(CLICKED_CLASS);
+    // } else {
+    //     title.classList.add(CLICKED_CLASS);
+    // }
+    title.classList.toggle(CLICKED_CLASS);//same code with above code
 }
 
 function init(){
