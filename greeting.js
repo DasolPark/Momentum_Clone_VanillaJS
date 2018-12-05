3434753ca2b6e3f25a456eb48566943513d0f72f
Dasol Park <dasol.park615@gmail.com>
2018-12-05 23:23:29 +0900

    [Make Momentum] 3.4 Saving the User Name part Two

--- greeting.js
+++ greeting.js
@@ -2,8 +2,24 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser";
-const SHOWING_CN = "showing";
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
@@ -14,7 +30,7 @@ function paintGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-        //he is not
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
