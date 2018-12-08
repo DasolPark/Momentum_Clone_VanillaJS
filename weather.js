e322ff389b04c86b98b1f5fa931577debd31aa32
Dasol Park <dasol.park615@gmail.com>
2018-12-09 00:10:16 +0900

    [Make Momentum] 3.10 Getting the Weather part Two API

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
-const COORDS = 'coords';
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "fc7c624b1d86597b7117e136e9223924";
+const COORDS = "coords";
+
+function getWeather(lat, lon){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,10 +27,11 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
-    console.log("cant access geo location");
+    console.log("can't access geo location");
 }
 
 function askForCoords(){
@@ -27,7 +43,8 @@ function askForCoords(){
      if(loadedCoords === null){
          askForCoords();
      } else {
-         //not
+         const parseCoords = JSON.parse(loadedCoords);
+         getWeather(parseCoords.latitude, parseCoords.longitude);
      }
  }
 
